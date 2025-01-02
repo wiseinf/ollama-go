5914cbc2742b8cfa60a893ad9502d5366b25fc57
JayLiu <liujian@wiseinf.com>
2025-01-02 18:16:01 +0800

    refactory: remove unnecessary comments.

--- ratelimit.go
+++ ratelimit.go
@@ -7,12 +7,10 @@ import (
 	"golang.org/x/time/rate"
 )
 
-// RateLimiter 接口定义速率限制行为
 type RateLimiter interface {
 	Wait() error
 }
 
-// rateLimiter 实现令牌桶算法的速率限制
 type rateLimiter struct {
 	limiter *rate.Limiter
 }
